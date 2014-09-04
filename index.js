9eaa5f0956040cba09abb27576524f03f21a73de
Keith Yao <i@yaofur.com>
2014-09-04 21:59:39 +0800

    no use coffee-script

--- index.js
+++ index.js
@@ -1,4 +1,50 @@
-require('coffee-script/register');
+/*
+ * Gulp-OZJS: gulp task for ozma
+ *
+ * Copyright (C) 2014, Keith Yao, MIT License
+ * vim: et:ts=2:sw=2:sts=2
+ */
 
-module.exports = require('./src/index');
+var gutil, ozma, path, print, root, through;
 
+print = console.log;
+
+path = require('path');
+
+root = path.resolve(__dirname, '../example');
+
+ozma = require('ozma').ozma;
+
+gutil = require('gulp-util');
+
+through = require('through2');
+
+module.exports = function(opts) {
+  return through.obj(function(file, enc, callback) {
+    var contents, onWrite;
+    if (!file.isBuffer()) {
+      this.push(file);
+      return callback();
+    }
+    contents = file.contents.toString('utf8');
+    onWrite = (function(_this) {
+      return function(output, output_code, output_mods, successCallback) {
+        var joinedFile;
+        joinedFile = new gutil.File({
+          base: opts.baseUrl,
+          path: output,
+          contents: new Buffer(output_code)
+        });
+        _this.push(joinedFile);
+        return successCallback();
+      };
+    })(this);
+    return ozma({
+      pipe: onWrite,
+      success: callback
+    }).exec({
+      config: opts,
+      _: [file.path]
+    }, callback);
+  });
+};
